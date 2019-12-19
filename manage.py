c4b6297cf53f227f3506738a19c689d567b1b544
Sophia Brandt <git@s-br.net>
2019-12-19 18:48:39 +0100

    🐛 FIX: minor fixes

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-import sys
-
 from flask.cli import FlaskGroup
 
 from project import create_app, db
@@ -12,9 +10,9 @@ cli = FlaskGroup(create_app=create_app)
 @cli.command("recreate_db")
 def recreate_db():
     """Recreates the database."""
+    db.session.remove()
     db.drop_all()
     db.create_all()
-    db.session.commit()
 
 
 @cli.command("seed_db")
