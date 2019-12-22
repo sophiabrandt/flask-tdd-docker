b4178e8ea25fced98102daad28a97b7f1dbc048f
Sophia Brandt <git@s-br.net>
2019-12-22 07:36:26 +0100

    📦 NEW: update user

--- project/api/users.py
+++ project/api/users.py
@@ -76,6 +76,31 @@ class Users(Resource):
         except ValueError:
             return response_object, 404
 
+    def put(self, user_id):
+        post_data = request.get_json()
+        response_object = {"status": "fail", "message": "Invalid payload."}
+        if not post_data:
+            return response_object, 400
+        username = post_data.get("username")
+        email = post_data.get("email")
+        if not username or not email:
+            return response_object, 400
+        try:
+            user = User.query.filter_by(id=int(user_id)).first()
+            if user:
+                user.username = username
+                user.email = email
+                db.session.commit()
+                response_object["status"] = "success"
+                response_object["message"] = f"{user.id} was updated!"
+                return response_object, 200
+            else:
+                response_object["message"] = "User does not exist."
+                return response_object, 404
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response_object, 400
+
 
 api.add_resource(UsersList, "/users")
 api.add_resource(Users, "/users/<user_id>")
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -127,3 +127,58 @@ def test_remove_user_incorrect_id(test_app, test_database):
     assert resp.status_code == 404
     assert "User does not exist." in data["message"]
     assert "fail" in data["status"]
+
+
+def test_update_user(test_app, test_database):
+    user = add_user("user-to-be-updated", "user-to-be-updated@test.com")
+    client = test_app.test_client()
+    resp_one = client.put(
+        f"/users/{user.id}",
+        data=json.dumps({"username": "me", "email": "me@test.com"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert f"{user.id} was updated!" in data["message"]
+    assert "success" in data["status"]
+    resp_two = client.get(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "me" in data["data"]["username"]
+    assert "me@test.com" in data["data"]["email"]
+    assert "success" in data["status"]
+
+
+def test_update_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put("/users/1", data=json.dumps({}), content_type="application/json",)
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Invalid payload." in data["message"]
+    assert "fail" in data["status"]
+
+
+def test_update_user_invalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put(
+        "/users/1",
+        data=json.dumps({"username": "john"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Invalid payload." in data["message"]
+    assert "fail" in data["status"]
+
+
+def test_update_user_does_not_exist(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put(
+        f"/users/999",
+        data=json.dumps({"username": "me", "email": "me@test.com"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "User does not exist." in data["message"]
+    assert "fail" in data["status"]
